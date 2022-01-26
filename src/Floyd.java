ba8689eb661d447180e7db0d4e37e380fbb4cb62
JavaConterry <conterry05@gmail.com>
2022-01-26 09:24:36 +0200

    fixes

--- src/Floyd.java
+++ src/Floyd.java
@@ -3,14 +3,20 @@ public class Floyd {
     private int[][] matrix;
 
     public Floyd(int[][] adjecencyMatrix){
-        matrix = adjecencyMatrix;
+        int length = adjecencyMatrix.length;
+        matrix = new int[length][];
+        for(int i=0; i<length; i++){
+            for(int j=0; j<length; j++){
+                matrix[i][j] = adjecencyMatrix[i][j];
+            }
+        }
     }
 
 
     public void updateDistances(){
-        for(int i=0; i<matrix.length; i++){
-            for(int j=0; j<matrix.length; j++){
-                for(int k=0; k<matrix.length; k++){
+        for(int k=0; k<matrix.length; k++){
+            for(int i=0; i<matrix.length; i++){
+                for(int j=0; j<matrix.length; j++){
                     int twoVerticles = matrix[i][k]+matrix[k][j];
                     if(twoVerticles < matrix[i][j]){
                         matrix[i][j] = twoVerticles;
